8ab5dc76f157c98278f3b34288b0467b75c1d1af
MYBLOGMARTHA <56209138+MYBLOGMARTHA@users.noreply.github.com>
2019-10-07 12:04:27 -0400

    Add files via upload

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+    <h1>About </h1>
+    <p>
+        Welcome to my blog!</p>
+
+
+         <p>“It’s never too late to be who you might have been.” – George Eliot</p>
+
+         <p>"Success is going from failure to failure without losing enthusiasm.” – Winston Churchill</p>
+
+         <p>“What we see depends mainly on what we look for.” – John Lubbock </p>
+    <p>
+        Please feel free to add comments, information and coding tips.  We all need a place to learn from each other.
+    </p>
+    <p>
+    
+    </p>
+    </>
+);
+
+export default AboutPage;
